6de3bc7af7a44e89301b79fa2df9a1c928cb35f4
recklessbunny <114820147+recklessbunny@users.noreply.github.com>
2023-06-11 19:32:30 -0600

    Update DomainJoin.ps1

--- Scripts/PowerShell/DomainJoin.ps1
+++ Scripts/PowerShell/DomainJoin.ps1
@@ -1,8 +1,8 @@
 ﻿Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses 10.1.0.4
 
-$domain='oftp.local'
+$domain='onlyfortestpurposes.online'
 $userName='oftp\lekkersap'
 $password='Lekk@.0102##$$' | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $password
+$credential=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $password
 
 Add-Computer -ComputerName (hostname) -DomainName $domain -Credential $credential -Restart
