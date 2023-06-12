086941d140b39c3d7fe3fdbac8ac5d70e6072b4b
recklessbunny <114820147+recklessbunny@users.noreply.github.com>
2023-06-12 07:22:30 -0600

    Update DomainJoin.ps1

--- Scripts/PowerShell/DomainJoin.ps1
+++ Scripts/PowerShell/DomainJoin.ps1
@@ -1,10 +1,10 @@
 ## This first command will set your interface DNS Address to the DC Address. ##
-Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses 10.1.0.4
+Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses 1.1.1.1
 
 ## This variables below will use the information declared as reference for the following commnad. ##
-$domain='onlyfortestpurposes.online'
-$userName='oftp\lekkersap'
-$password='Lekk@.0102##$$' | ConvertTo-SecureString -AsPlainText -Force
+$domain='domain.com'
+$userName='domain\user'
+$password='YourPassword' | ConvertTo-SecureString -AsPlainText -Force
 $credential=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $userName, $password
 
 
